eaaa0e8de3f930acecb00be47dc9f22fef6567f4
ellyhern <elias.hernandez5214@gmail.com>
2023-06-30 20:08:08 +0800

    remove: all code for wagmi

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { Route, Routes, Router, Navigate, BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PUBLIC_ROUTES } from './config/routes';
-import { AllProvider } from './Provider';
+// import { AllProvider } from './Provider';
 import { Home } from './pages/home';
 import { NotFoundPage } from './pages/404';
-import { Token } from './pages/token';
+// import { Token } from './pages/token';
 import { Verify } from './pages/verify';
 import { Layout } from 'src/layouts/layout';
 import './config/i18n';
@@ -15,26 +15,18 @@ function App() {
 
   return (
     <BrowserRouter>
-      {/* <SubstrateContextProvider> */}
       <WalletContextProvider>
         <Provider store={store}>
-          <AllProvider>
-            <Layout>
-              <Routes>
-                <Route path={PUBLIC_ROUTES.default} element={<Home />} />
-                <Route path={PUBLIC_ROUTES.home} element={<Home />} />
-                <Route path={PUBLIC_ROUTES.nft} element={<Token />} />
-                <Route path={PUBLIC_ROUTES.verify} element={<Verify />} />
-                {/* <Route path={PUBLIC_ROUTES.walletinfo} element={<WalletInfo />} /> */}
-                {/* <Route path={PUBLIC_ROUTES.privacy} element={<Privacy />} />
-              <Route path={PUBLIC_ROUTES.terms} element={<Terms />} /> */}
-                <Route path={PUBLIC_ROUTES.error404} element={<NotFoundPage />} />
-              </Routes>
-            </Layout>
-          </AllProvider>
+          <Layout>
+            <Routes>
+              <Route path={PUBLIC_ROUTES.default} element={<Home />} />
+              <Route path={PUBLIC_ROUTES.home} element={<Home />} />
+              <Route path={PUBLIC_ROUTES.verify} element={<Verify />} />
+              <Route path={PUBLIC_ROUTES.error404} element={<NotFoundPage />} />
+            </Routes>
+          </Layout>
         </Provider>
       </WalletContextProvider>
-      {/* </SubstrateContextProvider> */}
     </BrowserRouter >
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,90 +1,13 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import './index.css';
-// import 'aos/dist/aos.css';
-
-import { RainbowKitProvider, connectorsForWallets } from '@rainbow-me/rainbowkit';
-import {
-  argentWallet,
-  braveWallet,
-  imTokenWallet,
-  injectedWallet,
-  ledgerWallet,
-  metaMaskWallet,
-  omniWallet,
-  rainbowWallet,
-  trustWallet,
-  walletConnectWallet,
-  coinbaseWallet
-} from '@rainbow-me/rainbowkit/wallets';
-import { configureChains, createClient, WagmiConfig } from 'wagmi';
-import { bsc, polygonMumbai } from 'wagmi/chains';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
-
-// import '@rainbow-me/rainbowkit/styles.css';
-// import 'react-toastify/dist/ReactToastify.min.css';
-import { isTest } from './config/test';
-
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 window.Buffer = require('buffer').Buffer;
-
-const { chains, provider } = configureChains(
-  [isTest ? polygonMumbai : bsc],
-  [alchemyProvider({ apiKey: '6mDnh0_FqrDQzdcOCI_O5NkDs70x4VYp' }), publicProvider()]
-);
-
-const connectors = connectorsForWallets([
-  {
-    groupName: 'Inject your wallet',
-    wallets: [injectedWallet({ chains })]
-  },
-  {
-    groupName: 'Select your wallet',
-    wallets: [
-      metaMaskWallet({ chains, shimDisconnect: true }),
-      trustWallet({ chains, shimDisconnect: true }),
-      coinbaseWallet({ appName: 'Marmoset', chains })
-    ]
-  },
-  {
-    groupName: 'Others',
-    wallets: [
-      walletConnectWallet({ chains }),
-      ledgerWallet({ chains }),
-      braveWallet({ chains }),
-      argentWallet({ chains }),
-      imTokenWallet({ chains }),
-      omniWallet({ chains }),
-      rainbowWallet({ chains })
-    ]
-  }
-]);
-
-const wagmiClient = createClient({
-  connectors,
-  autoConnect: true,
-  provider
-});
-
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <WagmiConfig client={wagmiClient}>
-    <RainbowKitProvider
-      chains={chains}
-      showRecentTransactions
-      appInfo={{
-        appName: 'MarmosetClub'
-      }}
-    >
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </RainbowKitProvider>
-  </WagmiConfig>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
